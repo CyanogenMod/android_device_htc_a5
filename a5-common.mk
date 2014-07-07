6f76f67f0a38fadcabd57e080f9d7f3a39a527e0
Willi Ye <williye97@gmail.com>
2014-07-07 18:38:22 +0200

    remove some props
    Signed-off-by: Willi Ye <williye97@gmail.com>

--- a5-common.mk
+++ a5-common.mk
@@ -202,9 +202,6 @@ PRODUCT_COPY_FILES += \
 
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
-    rild.libargs=-d \
-    /dev/smd0 \
-    rild.libpath=/vendor/lib/libril-qc-qmi-1.so \
     ril.subscription.types=NV,RUIM \
     ro.baseband.arch=msm \
     ro.data.large_tcp_window_size=true \
