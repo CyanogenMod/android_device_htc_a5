56f8bc2fcc14aa2c6fc1159937a054a8c0467e89
Willi Ye <williye97@gmail.com>
2014-08-10 23:16:01 +0200

    fix media output and clean ramdisk
    Signed-off-by: Willi Ye <williye97@gmail.com>

--- device.mk
+++ device.mk
@@ -239,8 +239,16 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # AV
 PRODUCT_PROPERTY_OVERRIDES += \
+    media.stagefright.enable-player=true \
+    media.stagefright.enable-http=true \
+    media.stagefright.enable-aac=true \
+    media.stagefright.enable-qcp=true \
+    media.stagefright.enable-fma2dp=true \
+    media.stagefright.enable-scan=true \
+    mmp.enable.3g2=true \
     mm.enable.smoothstreaming=true \
-    media.aac_51_output_enabled=true
+    media.aac_51_output_enabled=true \
+    mm.enable.qcom_parser=37491
 
 # GPS
 PRODUCT_PROPERTY_OVERRIDES += \
