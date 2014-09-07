1c08a978987655de0eb38b3238f7ee0862926b94
Willi Ye <williye97@gmail.com>
2014-09-07 21:21:18 +0200

    Revert "Revert "switch to open source ril and update prima""
    This reverts commit d8adf1bec1ab071e7ad3faa548198aa0e19ff2d5.

---
+++ libril/Android.mk
@@ -0,0 +1,39 @@
+# Copyright 2006 The Android Open Source Project
+
+ifeq ($(BOARD_PROVIDES_LIBRIL),true)
+ifeq ($(TARGET_BOARD_PLATFORM),msm8226)
+ifeq ($(BOARD_VENDOR),htc)
+
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+    ril.cpp \
+    ril_event.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    libutils \
+    libbinder \
+    libcutils \
+    libhardware_legacy \
+    librilutils
+
+LOCAL_CFLAGS :=
+
+ifeq ($(BOARD_RIL_NO_CELLINFOLIST),true)
+LOCAL_CFLAGS += -DRIL_NO_CELL_INFO_LIST
+endif
+
+ifeq ($(BOARD_RIL_FIVE_SEARCH_RESPONSES),true)
+LOCAL_CFLAGS += -DRIL_FIVE_SEARCH_RESPONSES
+endif
+
+LOCAL_MODULE:= libril
+
+LOCAL_LDLIBS += -lpthread
+
+include $(BUILD_SHARED_LIBRARY)
+
+endif # BOARD_VENDOR
+endif # TARGET_BOARD_PLATFORM
+endif # BOARD_PROVIDES_LIBRIL
