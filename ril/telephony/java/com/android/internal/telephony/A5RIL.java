31af28817263d0ff5652c52d49ec943c93deb30e
Willi Ye <williye97@gmail.com>
2014-09-15 12:57:35 +0200

    add some constants from HTCQualcommRIL
    Signed-off-by: Willi Ye <williye97@gmail.com>

--- ril/telephony/java/com/android/internal/telephony/A5RIL.java
+++ ril/telephony/java/com/android/internal/telephony/A5RIL.java
@@ -6,23 +6,80 @@ import android.content.Context;
 import android.os.AsyncResult;
 import android.os.Message;
 import android.os.Parcel;
+import android.os.SystemProperties;
 import android.telephony.Rlog;
+import android.text.TextUtils;
 
+import com.android.internal.telephony.dataconnection.DataCallResponse;
 import com.android.internal.telephony.dataconnection.DataProfileOmh;
 import com.android.internal.telephony.dataconnection.DataProfile;
+import com.android.internal.telephony.dataconnection.DcFailCause;
 
 import java.util.ArrayList;
 
 public class A5RIL extends RIL implements CommandsInterface {
 
-    static final int RIL_REQUEST_HTC_GET_DATA_CALL_PROFILE = 5505;
-    static final int RIL_REQUEST_HTC_SET_UICC_SUBSCRIPTION = 5506;
-    static final int RIL_REQUEST_HTC_SET_DATA_SUBSCRIPTION = 5507;
-    static final int RIL_UNSOL_HTC_UICC_SUBSCRIPTION_STATUS_CHANGED = 5760;
+    private static final int RIL_UNSOL_ENTER_LPM = 3023;
+    private static final int RIL_UNSOL_CDMA_3G_INDICATOR = 4259;
+    private static final int RIL_UNSOL_CDMA_ENHANCE_ROAMING_INDICATOR = 4262;
+    private static final int RIL_UNSOL_CDMA_NETWORK_BASE_PLUSCODE_DIAL = 4270;
+    private static final int RIL_UNSOL_RESPONSE_PHONE_MODE_CHANGE = 4802;
+    private static final int RIL_UNSOL_RESPONSE_IMS_NETWORK_STATE_CHANGED = 1037;
+    private static final int RIL_UNSOL_RESPONSE_DATA_NETWORK_STATE_CHANGED = 5757;
+
+    private static final int RIL_REQUEST_HTC_GET_DATA_CALL_PROFILE = 5505;
+    private static final int RIL_REQUEST_HTC_SET_UICC_SUBSCRIPTION = 5506;
+    private static final int RIL_REQUEST_HTC_SET_DATA_SUBSCRIPTION = 5507;
+    private static final int RIL_UNSOL_HTC_UICC_SUBSCRIPTION_STATUS_CHANGED = 5760;
 
     public A5RIL(Context paramContext, int paramInt1,
            int paramInt2, Integer paramInteger) {
          super(paramContext, paramInt1, paramInt2, paramInteger);
+    }
+
+    @Override
+    protected Object
+    responseIccCardStatus(Parcel p) {
+        // force CDMA + LTE network mode
+        boolean forceCdmaLte = needsOldRilFeature("forceCdmaLteNetworkType");
+
+        if (forceCdmaLte) {
+            setPreferredNetworkType(NETWORK_MODE_LTE_CDMA_EVDO, null);
+        }
+
+        return super.responseIccCardStatus(p);
+    }
+
+    @Override
+    protected DataCallResponse getDataCallResponse(Parcel p, int version) {
+        DataCallResponse dataCall = new DataCallResponse();
+
+        dataCall.version = version;
+        dataCall.status = p.readInt();
+        dataCall.suggestedRetryTime = p.readInt();
+        dataCall.cid = p.readInt();
+        dataCall.active = p.readInt();
+        dataCall.type = p.readString();
+        dataCall.ifname = p.readString();
+        /* Check dataCall.active != 0 so address, dns, gateways are provided
+         * when switching LTE<->3G<->2G */
+        if ((dataCall.status == DcFailCause.NONE.getErrorCode()) &&
+                TextUtils.isEmpty(dataCall.ifname) && dataCall.active != 0) {
+            throw new RuntimeException("getDataCallResponse, no ifname");
+        }
+        String addresses = p.readString();
+        if (!TextUtils.isEmpty(addresses)) {
+            dataCall.addresses = addresses.split(" ");
+        }
+        String dnses = p.readString();
+        if (!TextUtils.isEmpty(dnses)) {
+            dataCall.dnses = dnses.split(" ");
+        }
+        String gateways = p.readString();
+        if (!TextUtils.isEmpty(gateways)) {
+            dataCall.gateways = gateways.split(" ");
+        }
+        return dataCall;
     }
 
     /**
@@ -91,7 +148,16 @@ public class A5RIL extends RIL implements CommandsInterface {
         int response = p.readInt();
 
         switch(response) {
+            case RIL_UNSOL_ENTER_LPM: ret = responseVoid(p); break;
+            case RIL_UNSOL_CDMA_3G_INDICATOR:  ret = responseInts(p); break;
+            case RIL_UNSOL_CDMA_ENHANCE_ROAMING_INDICATOR:  ret = responseInts(p); break;
+            case RIL_UNSOL_CDMA_NETWORK_BASE_PLUSCODE_DIAL:  ret = responseStrings(p); break;
+            case RIL_UNSOL_RESPONSE_PHONE_MODE_CHANGE:  ret = responseInts(p); break;
+            case RIL_UNSOL_RESPONSE_IMS_NETWORK_STATE_CHANGED: ret = responseVoid(p); break;
+            case RIL_UNSOL_RESPONSE_DATA_NETWORK_STATE_CHANGED: ret = responseVoid(p); break;
+            case RIL_UNSOL_RIL_CONNECTED: ret = responseInts(p); break;
             case RIL_UNSOL_HTC_UICC_SUBSCRIPTION_STATUS_CHANGED: ret = responseInts(p); break;
+
             default:
                 // Rewind the Parcel
                 p.setDataPosition(dataPosition);
@@ -102,6 +168,35 @@ public class A5RIL extends RIL implements CommandsInterface {
         }
 
         switch(response) {
+            case RIL_UNSOL_ENTER_LPM:
+            case RIL_UNSOL_CDMA_3G_INDICATOR:
+            case RIL_UNSOL_CDMA_ENHANCE_ROAMING_INDICATOR:
+            case RIL_UNSOL_CDMA_NETWORK_BASE_PLUSCODE_DIAL:
+            case RIL_UNSOL_RESPONSE_PHONE_MODE_CHANGE:
+            case RIL_UNSOL_RESPONSE_IMS_NETWORK_STATE_CHANGED:
+            case RIL_UNSOL_RESPONSE_DATA_NETWORK_STATE_CHANGED:
+                if (RILJ_LOGD) unsljLogRet(response, ret);
+
+                if (mExitEmergencyCallbackModeRegistrants != null) {
+                    mExitEmergencyCallbackModeRegistrants.notifyRegistrants(
+                                        new AsyncResult (null, null, null));
+                }
+                break;
+            case RIL_UNSOL_RIL_CONNECTED: {
+                if (RILJ_LOGD) unsljLogRet(response, ret);
+
+                // Initial conditions
+                if (SystemProperties.get("ril.socket.reset").equals("1")) {
+                    setRadioPower(false, null);
+                }
+                // Trigger socket reset if RIL connect is called again
+                SystemProperties.set("ril.socket.reset", "1");
+                setPreferredNetworkType(mPreferredNetworkType, null);
+                setCdmaSubscriptionSource(mCdmaSubscription, null);
+                setCellInfoListRate(Integer.MAX_VALUE, null);
+                notifyRegistrantsRilConnectionChanged(((int[])ret)[0]);
+                break;
+            }
             case RIL_UNSOL_HTC_UICC_SUBSCRIPTION_STATUS_CHANGED: {
                 if (RILJ_LOGD) unsljLogRet(response, ret);
 
