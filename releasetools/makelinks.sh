605bc631c7c81b40b15bc55ce6c8be0bf90bfca7
Willi Ye <williye97@gmail.com>
2014-09-16 20:11:21 +0200

    remove everything what has to do with nfc on dual sim variants

--- releasetools/makelinks.sh
+++ releasetools/makelinks.sh
@@ -14,7 +14,10 @@ esac
 if [ $variant == "dwg" ] || [ $variant == "dug" ]; then
 	rm -f /system/bin/rild
 	rm -f /system/lib/libril.so
-	find /system/etc/permissions -name "*nfc*" -exec rm -f {} \;
+	NFCFILES="app/NfcNci.apk etc/permissions/android.hardware.nfc.xml etc/permissions/android.hardware.nfc.hce.xml etc/permissions/com.android.nfc_extras.xml etc/permissions/com.cyanogenmod.nfc.enhanced.xml etc/nfcee_access.xml etc/libnfc-nxp.conf etc/libnfc-brcm.conf lib/libnfc-nci.so lib/hw/nfc_nci.a5.so lib/libnfc_nci_jni.so lib/libnfc_ndef.so framework/com.android.nfc_extras.jar priv-app/Tag.apk"
+	for i in $NFCFILES; do
+		rm -f /system/$i
+	done
 fi
 
 basedir="/system/blobs/$variant/"
