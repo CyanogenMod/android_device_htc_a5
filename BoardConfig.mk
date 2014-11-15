c5bc8baabd7e42aa697ba23a27a2f0a108781314
Willi Ye <williye97@gmail.com>
2014-11-15 18:42:00 +0100

    a5: Set selinux to permissive * Otherwise camera wrapper doesn't want to load camera vendor hal
    Change-Id: I1ed7bdd3914f3cb261d240a21f359c3f7d37f9e1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,7 +44,7 @@ TARGET_CPU_VARIANT := cortex-a7
 COMMON_GLOBAL_CFLAGS += -DHTCLOG
 
 # Kernel
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 ehci-hcd.park=3
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 ehci-hcd.park=3 androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
