3089189366d0f5101015b9bd689f9981c615d890
Ethan Chen <intervigil@gmail.com>
2014-11-19 19:28:28 +0100

    a5: Add missing qcmediaplayer package
    Change-Id: I1e0b43c74f086afaf3bf59761bbcddda783df1aa

--- device.mk
+++ device.mk
@@ -183,7 +183,11 @@ PRODUCT_PACKAGES += \
     libOmxEvrcEnc \
     libOmxQcelp13Enc \
     libstagefrighthw \
-    libdashplayer
+    libdashplayer \
+    qcmediaplayer
+
+PRODUCT_BOOT_JARS += \
+    qcmediaplayer
 
 # Power
 PRODUCT_PACKAGES += \
