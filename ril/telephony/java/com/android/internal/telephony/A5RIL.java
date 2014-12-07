4145132c29b99c5bf4876966a29bcdaad8fff26a
Vaibhav Bhasin <bhasinvaibhav97@gmail.com>
2014-12-07 11:59:43 +0100

    a5: ril: Set mQANElements to 5 * Fixes manual network searching
    Change-Id: I7542387d844cc8491e95a45f9ecb7e6177ea96b6

--- ril/telephony/java/com/android/internal/telephony/A5RIL.java
+++ ril/telephony/java/com/android/internal/telephony/A5RIL.java
@@ -23,6 +23,7 @@ public class A5RIL extends RIL implements CommandsInterface {
     public A5RIL(Context paramContext, int paramInt1,
            int paramInt2, Integer paramInteger) {
          super(paramContext, paramInt1, paramInt2, paramInteger);
+         mQANElements = 5;
     }
 
     @Override
