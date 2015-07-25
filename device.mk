CyanogenMod/android_device_htc_m8

releasetools: s/makelinks/variant_script/
* this is more descriptive of what this script does

Change-Id: I91147d7778715cae1ac525856f76ccf43761b91a

device.mk
@@ -215,7 +215,7 @@ PRODUCT_PACKAGES += \
 
 # Variant linking script
 PRODUCT_COPY_FILES += \
-    device/htc/m8/releasetools/makelinks.sh:install/bin/makelinks.sh
+    device/htc/m8/releasetools/variant_script.sh:install/bin/variant_script.sh
 
 # Wifi firmware
 PRODUCT_PACKAGES += \

CyanogenMod/android_device_htc_a5

device.mk
@@ -195,7 +195,7 @@ PRODUCT_PACKAGES += \
 
 # Variant linking script
 PRODUCT_COPY_FILES += \
-    device/htc/a5/releasetools/makelinks.sh:install/bin/makelinks.sh
+    device/htc/a5/releasetools/variant_script.sh:install/bin/variant_script.sh
 
 # Wifi firmware
 PRODUCT_PACKAGES += \